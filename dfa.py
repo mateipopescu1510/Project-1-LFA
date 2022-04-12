515d73ce7cf617a0903bd10db9f5d080755cf209
Matei Popescu <popescumateicalin1510@gmail.com>
2022-04-12 11:11:48 +0300

    Final version

--- dfa.py
+++ dfa.py
@@ -7,29 +7,31 @@ class dfa:
             nodeInfo.append(i)
             nodeInfo.append(False)
             self.graph.append(nodeInfo)
-
-    def put(self):
-        print(self.graph)
+            # [index, False]
 
     def addEdge(self, edge):
+        # edge[1] = next index
+        # edge[2] = letter
         nextNode = (edge[1], edge[2])
         self.graph[edge[0]].append(nextNode)
+        # [index, bool, (nextIndex, letter)]
 
     def setInitialState(self, q):
         self.initialState = q
 
     def finalState(self, q):
         self.graph[q][1] = True
+        # [index, False, ...] -> [index, True, ...]
 
     def checkNextLetter(self, q, word, path):
         if not word:
-            if self.graph[q][1]:
+            if self.graph[q][1]: # if current state is final
                 path.append(q)
                 return path
             return False
-        if len(self.graph[q]) == 2:
-            return False
-        for edge in range(2, len(self.graph[q])):
+        if len(self.graph[q]) == 2: # if the word still has letters but there are no more next edges
+            return False 
+        for edge in range(2, len(self.graph[q])): # all edges
             if word[0] == self.graph[q][edge][1]:
                 newPath = path
                 newPath.append(q)
@@ -47,6 +49,7 @@ class dfa:
         else:
             print(result)
 
+
 cin = open("input.txt")
 
 init = cin.readline().split()
@@ -55,7 +58,7 @@ numberOfEdges = int(init[1])
 
 Graph = dfa(numberOfNodes)
 
-for i in range(numberOfEdges):
+for e in range(numberOfEdges):
     edge = cin.readline().split()
     edge[0], edge[1] = int(edge[0]), int(edge[1])
     Graph.addEdge(edge)
